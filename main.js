cff2e3df0aad83f346c2e04bcd6372d46dadac25
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2026-01-29 15:29:03 -0500

    spinning cube

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 import * as THREE from 'three';
+//npx vite to run website
+
+const width = window.innerWidth, height = window.innerHeight;
+
+//init
+
+const camera = new THREE.PerspectiveCamera(70, width/height, 0.01, 10);
+camera.position.z = 1;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(width, height);
+renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+//animation
+function animate(time) {
+    mesh.rotation.x = time/2000;
+    mesh.rotation.y = time/1000;
 
     renderer.render(scene, camera);
 }
-
-animate();
