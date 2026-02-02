1d458a559d346f47e99c8f8dee5d5558e50fc365
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2026-02-02 12:40:22 -0500

    transparent background ooo

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
 import * as THREE from 'three';
-//npx vite to run website
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 
-const width = window.innerWidth, height = window.innerHeight;
+//npx vite build
 
-//init
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
 
-const camera = new THREE.PerspectiveCamera(70, width/height, 0.01, 10);
-camera.position.z = 1;
+const scene =  new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, 0.1, 1000);
 
-const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
+// const renderer = new THREE.WebGLRenderer();
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    alpha: true,
+    premultipliedAlpha: false,
+    antialias: true
+});
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+function main() {
+    renderer.setSize(WIDTH*0.7, HEIGHT*0.7);
+    //this adds the thing to the body
+    document.body.appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(width, height);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+    scene.add(cube);
 
-//animation
-function animate(time) {
-    mesh.rotation.x = time/2000;
-    mesh.rotation.y = time/1000;
+    //add french horn
+    // {
+    //     const objLoader = new OBJLoader();
+    //     objLoader.load('/models/frenchHorn.obj', (root) => {
+    //         scene.add(root);
+    //     });
+    // }
 
-    renderer.render(scene, camera);
+    camera.position.z = 5;
+
+    renderer.setAnimationLoop(animate);
 }
+
+function animate() {
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+}
+
+main();
